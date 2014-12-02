5a546bc4636ce15b4ba3361247293a00f6cd6ca8
paulforbes42 <paul@factgem.com>
2014-12-01 22:32:27 -0500

    Client updates and spectator join game events

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,15 @@
       $('#event-log').append('<li>Specator <em>'+spectator+'</em> left</li>');
     });
 
-    socket.on('player-join-team', function(data) {
-        //$('#'+data.team).find('.
+    socket.on('when spectator wants to play as team', function(data) {
+        $('.'+data).find('h5')[0].innerHTML = "Player: "+data;
+        $('.'+data).find('button').addClass('hide');
+    });
+
+    $(document).ready(function() {
+        $('.team').find('button').click(function(e) {
+        console.log('test', e.target.value);
+            socket.emit('when spectator wants to play as team', e.target.value);
+        });
     });
 })();
